55d2af2e7fea9b38f68307b23864e544702e6b3c
vivekkumar2909 <rajvivek620065@gmail.com>
2024-03-30 14:39:08 +0530

    Validation Function Create

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
@@ -5,11 +6,12 @@ import { moderateScale, moderateVerticalScale, verticalScale } from 'react-nativ
 import { BG_COLOR } from '../utils/Colors';
 import { TextInput } from 'react-native-gesture-handler';
 
-const CustomInput = ({title,placeholder}) => {
+
+const CustomInput = ({ title, placeholder, value, onChangeText, bad }) => {
   return (
-    <View style={styles.input}>
-      <Text style={styles.title}>{title}</Text>
-      <TextInput placeholder={placeholder}/>
+    <View style={[styles.input, { borderColor: bad ? 'red' : '#9e9e9e' }]}>
+      <Text style={[styles.title, { color: bad ? 'red' : 'black' }]}>{title}</Text>
+      <TextInput value={value} onChangeText={text => onChangeText(text)} placeholder={placeholder} />
     </View>
   );
 };
@@ -17,23 +19,23 @@ const CustomInput = ({title,placeholder}) => {
 export default CustomInput;
 
 const styles = StyleSheet.create({
-    input:{
-        width:'90%',
-        height:verticalScale(45),
-        borderWidth : 0.4,
-        alignSelf: 'center',
-        marginTop: moderateVerticalScale(20),
-        borderRadius: moderateScale(10),
-        justifyContent: 'center',
-        paddingHorizontal: moderateScale(15),
-    },
-    title:{
-        alignSelf:'flex-start',
-        marginLeft:moderateScale(20),
-        top: -moderateVerticalScale(8),
-        position:'absolute',
-        backgroundColor: BG_COLOR,
-        paddingHorizontal: moderateScale(10),
-        color : '#000',
-      },
+  input: {
+    width: '90%',
+    height: verticalScale(45),
+    borderWidth: 0.4,
+    alignSelf: 'center',
+    marginTop: moderateVerticalScale(20),
+    borderRadius: moderateScale(10),
+    justifyContent: 'center',
+    paddingHorizontal: moderateScale(15),
+  },
+  title: {
+    alignSelf: 'flex-start',
+    marginLeft: moderateScale(20),
+    top: -moderateVerticalScale(8),
+    position: 'absolute',
+    backgroundColor: BG_COLOR,
+    paddingHorizontal: moderateScale(10),
+    color: '#000',
+  },
 });
--- src/screens/jobposting/SignUpForCampany.js
+++ src/screens/jobposting/SignUpForCampany.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable prettier/prettier */
 import { Text, StyleSheet, Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { BG_COLOR } from '../../utils/Colors';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { moderateScale, moderateVerticalScale, scale } from 'react-native-size-matters';
@@ -11,22 +12,157 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
 const SignUpForCompany = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation('');
+  const nameRegex = /^[A-Za-z]+$/;
+  const EmailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  const [name, setName] = useState('');
+  const [badname, setBadname] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [badEmail, setBadEmail] = useState('');
+
+  const [contact, setContact] = useState('');
+  const [badContact, setBadContact] = useState('');
+
+  const [companyName, setCompanyName] = useState('');
+  const [badCompanyName, setBadCompanyName] = useState('');
+
+  const [companyAddress, setCompanyAddress] = useState('');
+  const [badCompanyAddress, setBadCompanyAddress] = useState('');
+
+  const [password, setPassword] = useState('');
+  const [badPassword, setBadPassword] = useState('');
+
+  const validate = () => {
+    let valideEmail = true;
+    let valideName = true;
+    let valideContact = true;
+    let valideCompanyName = true;
+    let valideCompanyAddress = true;
+    let validePassword = true;
+    if (name === '') {
+      valideName = false;
+      setBadname('Please Enter Name');
+    }
+    else if (name !== '' && name.length < 3) {
+      valideName = false;
+      setBadname('Please Enter Valid Name');
+    }
+    else if (name !== '' && name.length >= 3 && !name.toString().match(nameRegex)) {
+      valideName = false;
+      setBadname('Please Enter Valid Name');
+    }
+    else if (name !== '' && name.length >= 3 && name.toString().match(nameRegex)) {
+      valideName = true;
+      setBadname('');
+    }
+    if (email === '') {
+      valideEmail = false;
+      setBadEmail('Please Enter Email');
+    }
+
+    else if (email !== '' && email.length >= 3 && !email.toString().match(EmailRegex)) {
+      valideEmail = false;
+      setBadEmail('Please Enter Valid Email');
+    }
+    else if (name !== '' && email.length >= 3 && email.toString().match(EmailRegex)) {
+      valideName = true;
+      setBadEmail('');
+    }
+
+    const contactRegex = /^\d+$/;
+    if (contact === '') {
+      valideContact = false;
+      setBadContact('Please Enter Contact Number');
+    }
+
+    else if (contact !== '' && contact.length < 10) {
+      valideContact = false;
+      setBadContact('Please Enter Valid Contact Number');
+    }
+    else if (contact !== '' && contact.length >= 10 && !contact.toString().match(contactRegex)) {
+      valideContact = false;
+      setBadContact('Please Enter Valid Contact Number');
+    }
+    else if (contact !== '' && contact.length >= 10 && contact.toString().match(contactRegex)) {
+      valideContact = true;
+      setBadContact('');
+    }
+
+    if (companyName ==='')
+    {
+      valideCompanyName = false;
+      setBadCompanyName('Please Enter Company Name');
+    }
+    else if (companyName !== '' ) {
+      valideCompanyName = false;
+      setBadCompanyName('');
+    }
+
+    if (companyAddress ==='')
+    {
+      valideCompanyAddress = false;
+      setBadCompanyAddress('Please Enter Company Address');
+    }
+    else if (companyAddress !== '' ) {
+      valideCompanyAddress = false;
+      setBadCompanyAddress('');
+    }
+
+    if (password ==='')
+    {
+      validePassword = false;
+      setBadPassword('Please Enter Password');
+    }
+    else if (password !== '' && password.length < 6) {
+      validePassword = false;
+      setBadPassword('Please Enter Minimum 6 Character Password');
+    }
+    else if (password !== '' && password.length >= 6 && password.toString().match(nameRegex)) {
+      validePassword = true;
+      setBadPassword('');
+    }
+
+    return validePassword && valideName && valideEmail && valideCompanyAddress && valideCompanyName && valideContact;
+
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Image source={require('../../images/logo.png')} style={styles.logo} />
         <Text style={styles.title}>Create Account </Text>
-        <CustomInput title={'Name'} placeholder={'xyz'} />
-        <CustomInput title={'Email'} placeholder={'xyz@gmail.com'} />
-        <CustomInput title={'Contact'} placeholder={'+91 96089*****'} />
-        <CustomInput title={'Company Name'} placeholder={'ex google, MicroSoft'} />
-        <CustomInput title={'Company Name'} placeholder={'ex google, MicroSoft'} />
-        <CustomInput title={'Address'} placeholder={'ex. Delhi'} />
-        <Text style={styles.forgotPassword}> Forgot Password?</Text>
-        <CustomSolidBtn title={'Sign Up'} />
-        <CustomBorderBtn title={'Log In'} onClick={()=>
-        navigation.goBack()}/>
+        <CustomInput
+          value={name}
+          onChangeText={txt => { setName(txt); }}
+          title={'Name'}
+          placeholder={'xyz'}
+          bad={badname !== '' ? true : false}
+        />
+
+        {badname !== '' && <Text style={styles.errMsg}>{badname}</Text>}
+        <CustomInput
+          value={email}
+          onChangeText={txt => { setEmail(txt); }}
+          title={'Email'} placeholder={'xyz@gmail.com'} bad={badEmail !== '' ? true : false} />
+          {badEmail !== '' && <Text style={styles.errMsg}>{badEmail}</Text>}
+        <CustomInput value={contact}
+          onChangeText={txt => { setContact(txt); }} title={'Contact'} placeholder={'+91 96089*****'} />
+        <CustomInput value={companyName}
+          onChangeText={txt => { setCompanyName(txt); }} title={'Company Name'} placeholder={'ex google, MicroSoft'} />
+        <CustomInput value={companyAddress}
+          onChangeText={txt => { setCompanyAddress(txt); }} title={'Address'} placeholder={'ex. Delhi'} />
+        <CustomInput value={password}
+          onChangeText={txt => { setPassword(txt); }} title={'Password'} placeholder={'********'} />
+        {/* <Text style={styles.forgotPassword}> Forgot Password?</Text> */}
+        <CustomSolidBtn title={'Sign Up'} onClick={() => {
+          if (validate()) {
+
+          }
+        }} />
+        <CustomBorderBtn title={'Log In'} onClick={() =>
+          navigation.goBack()} />
       </ScrollView>
     </SafeAreaView>
   );
@@ -60,5 +196,11 @@ const styles = StyleSheet.create({
     marginTop: moderateVerticalScale(15),
     color: '#000',
     fontSize: moderateScale(17),
-  }
+  },
+  errMsg: {
+    marginLeft: moderateScale(20),
+    color: 'red',
+    // fontSize: moderateScale(17),
+    marginTop: moderateVerticalScale(10),
+  },
 });
